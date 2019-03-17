5fd96f8722f831aae531e54f1420466866f86ffb
Yury Chetyrko <ychetyrko@gmail.com>
2019-03-17 18:55:54 +0300

    Publish GitHub Pages from master/docs (#1)

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     app: [path.resolve(__dirname, 'src', 'index.tsx')]
   },
   output: {
-    path: path.resolve(__dirname, './public'),
+    path: path.resolve(__dirname, './docs'),
     filename: '[name].js'
   },
   watchOptions: {
